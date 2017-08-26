874c0dc056302fd12bf26ab1802f2e5c9ed076d4
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-08-26 02:07:42 -0400

    Command with con_id, timeout with separate thread

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use i3ipc::event::Event;
 use i3ipc::event::inner;
 
 use std::env;
+use std::thread;
+use std::time::Duration;
 
 fn parse(args: &[String]) -> i32 {
   if args.len() == 1 {
@@ -25,6 +27,8 @@ fn parse(args: &[String]) -> i32 {
 fn main() {
   let args: Vec<String> = env::args().collect();
   let highlight = parse(&args);
+
+  let millis = 1000;
 
   let mut i3 = I3Connection::connect().ok().expect("Failed to connect");
   let mut listener = I3EventListener::connect().ok().expect("Failed to bind");
@@ -44,9 +48,17 @@ fn main() {
           inner::WindowChange::Focus => {
             // Ignore quake console
             if e.container.name.unwrap() != "quaketerm" {
-              // Clear all borders, add border to focused
+              // Clear all borders
               i3.command("[tiling] border none").unwrap();
-              i3.command(&format!("border pixel {}", highlight)).unwrap();
+
+              // Highlight
+              let id = e.container.id;
+              i3.command(
+                &format!("[con_id=\"{}\"] border pixel {}", id, highlight)
+              ).unwrap();
+              //i3.command(&format!("border pixel {}", highlight)).unwrap();
+
+              deselect(id, millis);
             }
           }
           _ => { }
@@ -58,4 +70,12 @@ fn main() {
     }
     //println!("{:?}\n", ev.ok())
   }
+}
+
+fn deselect(id: i32, millis: u64) {
+  thread::spawn(move || {
+    let mut i3 = I3Connection::connect().ok().expect("Failed to connect");
+    thread::sleep(Duration::from_millis(millis));
+    i3.command(&format!("[con_id=\"{}\"] border none", id)).unwrap();
+  });
 }
