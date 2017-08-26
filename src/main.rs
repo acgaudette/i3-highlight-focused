d725e7b2cd9b2ec125cda7161c186818c99f54d5
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-08-26 04:21:53 -0400

    Reorder

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,15 @@
 // Created by Aaron C Gaudette on 25.08.17
 
+use std::env;
+use std::thread;
+use std::time::Duration;
+
 extern crate i3ipc;
 use i3ipc::I3Connection;
 use i3ipc::I3EventListener;
 use i3ipc::Subscription;
 use i3ipc::event::Event;
 use i3ipc::event::inner;
-
-use std::env;
-use std::thread;
-use std::time::Duration;
-
-fn parse(args: &[String]) -> (i32, u64) {
-  if args.len() < 2 {
-    panic!("Not enough arguments!");
-  }
-
-  let hi = match args[1].parse::<i32>() {
-    Ok(i) => i, Err(e) => {
-      panic!("Incorrect argument: {}", e);
-    }
-  };
-
-  let m = match args[2].parse::<u64>() {
-    Ok(i) => i, Err(e) => {
-      panic!("Incorrect argument: {}", e);
-    }
-  };
-
-  (hi, m)
-}
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -38,11 +18,7 @@ fn main() {
   let mut i3 = I3Connection::connect().ok().expect("Failed to connect");
   let mut listener = I3EventListener::connect().ok().expect("Failed to bind");
 
-  let subs = [
-    Subscription::Window,
-  ];
-
-  // Subscribe
+  let subs = [ Subscription::Window ];
   listener.subscribe(&subs).ok().expect("Failed to subscribe");
 
   for ev in listener.listen() {
@@ -62,9 +38,10 @@ fn main() {
                 &format!("[con_id=\"{}\"] border pixel {}", id, highlight)
               ).unwrap();
 
+              // Wait and remove highlight
               deselect(id, millis);
             }
-          }
+          },
           _ => { }
         }
       },
@@ -73,6 +50,26 @@ fn main() {
       _ => unreachable!()
     }
   }
+}
+
+fn parse(args: &[String]) -> (i32, u64) {
+  if args.len() < 2 {
+    panic!("Not enough arguments!");
+  }
+
+  let hi = match args[1].parse::<i32>() {
+    Ok(i) => i, Err(e) => {
+      panic!("Incorrect argument: {}", e);
+    }
+  };
+
+  let m = match args[2].parse::<u64>() {
+    Ok(i) => i, Err(e) => {
+      panic!("Incorrect argument: {}", e);
+    }
+  };
+
+  (hi, m)
 }
 
 fn deselect(id: i32, millis: u64) {
